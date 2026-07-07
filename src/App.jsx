37a3d401ac4e2fa41ddbdd1d9cda2d427749750d
ericsinghh <singheric890@gmail.com>
2026-07-07 23:41:47 +0530

    feat: scaffold routing and pages with react-router-dom configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Agence from './pages/Agence'
 
 const App = () => {
   return (
-    <div className='text-6xl text-white'> App </div>
+    <div className='text-white'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/projects' element={<Projects />} />
+
+      </Routes>
+    </div>
   )
 }
 
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Agence = () => {
   return (
     <div>
-      
+      <h1>Agence</h1>
     </div>
   )
 }
