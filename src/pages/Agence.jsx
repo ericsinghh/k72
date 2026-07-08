cbc7bfaf96d9e5449916a636a6dd06b85b7d27f6
ericsinghh <singheric890@gmail.com>
2026-07-09 01:09:04 +0530

    add GSAP dependencies and implement Agence page structure

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Agence = () => {
+
+  const imageDivRef = useRef(null);
+
   return (
     <div>
-      <div className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-40 left-[30vw] bg-red-500'>
-        <img className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+      <div className='section1'>
+        <div ref={imageDivRef} className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-96 left-[30vw] bg-red-500'>
+          <img className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+        </div>
+        <div className='relative font-[font2]'>
+          <div className='mt-[55vh]'>
+            <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>SEVEN7Y <br />
+              TWO</h1>
+          </div>
+          <div className='pl-[40%] mt-20'>
+            <p className='text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+          </div>
+        </div>
       </div>
-      <div className='relative font-[font2]'>
-        <div className='mt-[55vh]'>
-          <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>SEVEN7Y <br />
-            TWO</h1>
-        </div>
-        <div className='pl-[50%] mt-20'>
-          <p className='text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
-        </div>
+      <div className="section2 h-screen">
+
       </div>
     </div>
   )
