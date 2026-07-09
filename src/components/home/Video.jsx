1ad03941332ca2975ff6bb7f1c77e203c27e9a63
ericsinghh <singheric890@gmail.com>
2026-07-09 13:51:04 +0530

    add Agence page with GSAP scroll-triggered image transitions and reusable Video component

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div className='h-screen w-screen fixed'>
-      <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1783528006~exp=1783531606~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=2b88aef6a30b9cde7257989f897b931e7fdf26432d1bc0696ae6455924b0a2d3&r=dXMtZWFzdDE%3D"></video>
+    <div className='h-full w-full'>
+      <video className='h-full w-full object-cover' autoPlay loop muted src="/video.mp4"></video>
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,14 +1,58 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React, { useRef } from 'react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Agence = () => {
 
-  const imageDivRef = useRef(null);
+  gsap.registerPlugin(ScrollTrigger)
+
+  const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
+
+  const imageArray = [
+    'https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
+    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
+    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6',
+
+  ]
+
+  useGSAP(function () {
+
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: 'top 28%',
+        end: 'top -70%',
+        pin: true,
+        onUpdate: (element) => {
+
+          let imageIndex;
+
+          if (element.progress < 1) {
+            imageIndex = Math.floor(element.progress * imageArray.length)
+          } else {
+            imageIndex = imageArray.length - 1
+          }
+          imageRef.current.src = imageArray[imageIndex]
+        }
+      }
+    })
+  })
 
   return (
     <div>
       <div className='section1'>
-        <div ref={imageDivRef} className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-96 left-[30vw] bg-red-500'>
-          <img className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+        <div ref={imageDivRef} className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw]  top-96 left-[30vw]'>
+          <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
         </div>
         <div className='relative font-[font2]'>
           <div className='mt-[55vh]'>
@@ -16,7 +60,7 @@ const Agence = () => {
               TWO</h1>
           </div>
           <div className='pl-[40%] mt-20'>
-            <p className='text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+            <p className='text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
           </div>
         </div>
       </div>
