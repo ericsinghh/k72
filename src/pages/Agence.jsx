24432a9a53dd76b7ea6993aed9f2572aaea1abeb
ericsinghh <singheric890@gmail.com>
2026-07-11 19:02:58 +0530

    implement full-screen navigation with GSAP animations and routing setup

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -54,16 +54,16 @@ const Agence = () => {
   return (
     <div className='parent'>
       <div id='page1' className='py-1'>
-        <div ref={imageDivRef} className='absolute overflow-hidden h-[40vw] md:h-[20vw] rounded-3xl w-[30vw] md:w-[15vw] top-24 md:top-42 left-[35vw] md:left-[30vw]'>
+        <div ref={imageDivRef} className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw] lg:top-96 left-[30vw]'>
           <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
         </div>
         <div className='relative font-[font2]'>
-          <div className='mt-[35vh] md:mt-[55vh]'>
-            <h1 className='text-[14vw] md:text-[20vw] text-center uppercase leading-[12vw] md:leading-[18vw]'>SEVEN7Y <br />
+          <div className='lg:mt-[55vh] mt-[30vh]'>
+            <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>SEVEN7Y <br />
               TWO</h1>
           </div>
-          <div className='pl-0 md:pl-[42%] px-4 md:px-0 mt-4 md:mt-0.2'>
-            <p className='text-2xl md:text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+          <div className='pl-[40%] mt-20'>
+            <p className='text-6xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
           </div>
         </div>
       </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -19,6 +19,7 @@ const Projects = () => {
     image2: 'https://k72.ca/images/caseStudies/SHELTON/thumbnailimage_shelton.jpg?w=1280&h=960&s=63d0eaa180cbc02d3ada285ad9ef1479',
   }]
 
+
   useGSAP(function () {
     gsap.from('.hero', {
       height: '100px',
@@ -36,13 +37,15 @@ const Projects = () => {
   })
 
   return (
-    <div className='p-4 mb-[100vh]'>
-      <div className='pt-[30vh] md:pt-[45vh]'>
-        <h2 className='font-[font2] text-[14vw] md:text-[9.5vw] uppercase'>Projets</h2>
+    <div className='lg:p-4 p-2 mb-[100vh]'>
+      <div className='pt-[45vh]'>
+        <h2 className='font-[font2] lg:text-[9.5vw] text-7xl uppercase'>Projets</h2>
       </div>
-      <div className='-mt-10 md:-mt-20 lol'>
+      <div className='-lg:mt-20 lol'>
         {projects.map(function (elem, index) {
-          return <ProjectCard key={index} image1={elem.image1} image2={elem.image2} />
+          return <div key={index} className='hero w-full lg:h-[850px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
+            <ProjectCard image1={elem.image1} image2={elem.image2} />
+          </div>
         })}
 
       </div>
@@ -50,4 +53,4 @@ const Projects = () => {
   )
 }
 
-export default Projects
+export default Projects
